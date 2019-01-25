ae16ffa4d6d649a6129e0365442673ad3f52b122
Thomas Maschler <tmaschler@wri.org>
2019-01-24 22:46:12 -0500

    bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo yum install docker
+sudo yum install git
+
+sudo mkfs.ext4 -E nodiscard /dev/nvme1n1
+sudo mkdir -p /mnt/data
+sudo mount -o discard /dev/nvme1n1 /mnt/data
+
+git clone https://github.com/wri/glad_tiles_pipeline.git
+cd glad_tiles_pipeline
+sudo service docker start
+sudo docker build . -t glad-pipeline
+
+sudo docker run -it -v /mnt/data:/home/gfw/code/data glad-pipeline python3 /home/gfw/code/src/glad_pipeline.py
