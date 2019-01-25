60dd642d1e2f617444af4018a03a81ba858011de
Thomas Maschler <tmaschler@wri.org>
2019-01-24 23:06:51 -0500

    remove sudo from bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-sudo yum install docker
-sudo yum install git
+yum install docker
+yum install git
 
-sudo mkfs.ext4 -E nodiscard /dev/nvme1n1
-sudo mkdir -p /mnt/data
-sudo mount -o discard /dev/nvme1n1 /mnt/data
+mkfs.ext4 -E nodiscard /dev/nvme1n1
+mkdir -p /mnt/data
+mount -o discard /dev/nvme1n1 /mnt/data
 
 git clone https://github.com/wri/glad_tiles_pipeline.git
 cd glad_tiles_pipeline
-sudo service docker start
-sudo docker build . -t glad-pipeline
+service docker start
+docker build . -t glad-pipeline
 
-sudo docker run -it -v /mnt/data:/home/gfw/code/data glad-pipeline glad_pipeline.py --include_russia
+docker run -v /mnt/data:/home/gfw/code/data glad-pipeline glad_pipeline.py --include_russia
