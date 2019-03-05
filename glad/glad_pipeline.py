b9c2504d5b2a848ddf53365a84f98cb891c80a63
Thomas Maschler <tmaschler@wri.org>
2019-03-05 13:07:29 -0500

    HOTFIX emissions path

--- glad/glad_pipeline.py
+++ glad/glad_pipeline.py
@@ -66,7 +66,7 @@ def main():
             "db_table": "tile_alert_stats",
         },
         "paths": {
-            "emissions": "s3://gfw2-data/climate/WHRC_biomass/WHRC_V4/t_co2_pixel/{top}_{left}_mt_co2_pixel_2000.tif",
+            "emissions": "s3://gfw2-data/climate/WHRC_biomass/WHRC_V4/t_co2_pixel/{top}_{left}_t_co2_pixel_2000.tif",
             "climate_mask": "s3://gfw2-data/forest_change/umd_landsat_alerts/archive/pipeline/climate/climate_mask/climate_mask_{top}_{left}.tif",
             "preprocessed": "s3://gfw2-data/forest_change/umd_landsat_alerts/archive/tiles/{tile_id}/{product}{year}.tif",
             #  "stats_db": "s3://palm-risk-poc/data/mvt/stats.db",
